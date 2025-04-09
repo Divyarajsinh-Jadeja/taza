717e9734ba1f28d325d7669f511d3de1255a6776
Divyarajsinh <divyarajsinh.jadeja@bytestechnolab.com>
2025-04-09 13:13:59 +0530

    - Export File Changes

--- lib/app/app_const.dart
+++ lib/app/app_const.dart
@@ -5,7 +5,7 @@ class AppConst {
 
   static Size designSize = const Size(390, 844);
   static const double defaultScrollControlDisabledMaxHeightRatio = 9.0 / 16.0;
-  static const String appName = 'ERP App';
+  static const String appName = 'Tazza';
   static const String themeDark = 'dark';
   static const String themeLight = 'light';
   static const String themeSystem = 'system';
